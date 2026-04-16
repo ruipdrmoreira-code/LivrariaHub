4cf6d6d7b1e80fb0fe2031b9eae6ff98df8eb4ac
K4ir0z <kai.magalhaes.324268777@msft.cesae.pt>
2026-04-16 11:44:47 +0100

    f

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 // -------------------------DARK/LIGHTMODE---------------------------
 const html = document.documentElement;
-const dark = document.querySelector('#temaDark');
-const light = document.querySelector('#temaLight');
+const dark = document.querySelector("#temaDark");
+const light = document.querySelector("#temaLight");
 
 async function mudarTema(t) {
-    html.setAttribute('data-bs-theme', t);
+  html.setAttribute("data-bs-theme", t);
 }
 
-dark.addEventListener('click', () => mudarTema('dark'));
-light.addEventListener('click', () => mudarTema('light'));
-// --------------------------------------------------------------------------
+dark.addEventListener("click", () => mudarTema("dark"));
+light.addEventListener("click", () => mudarTema("light"));
+
+// ------------------------------Filtro Pesquisa NOME ESCOLAS--------------------------------------------
+
+document.addEventListener("DOMContentLoaded", () => {
+  const campoEscola = document.getElementById("PesquisaEscola");
+
+  const filtrarEscolas = () => {
+    const termoBusca = campoEscola.value.toLowerCase().trim();
+    const itensParaFiltrar = document.querySelectorAll(".item-livro");
+
+    itensParaFiltrar.forEach((item) => {
+    
+      const nomeEscolaNoCard = item
+        .querySelector(".nome-escola")
+        .textContent.toLowerCase();
+
+      if (nomeEscolaNoCard.includes(termoBusca)) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  };
+
+  if (campoEscola) {
+    campoEscola.addEventListener("input", filtrarEscolas);
+  }
+});
