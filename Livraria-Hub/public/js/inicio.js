5085f145f07b6ca41ca7806b23f31b70495ef59a
K4ir0z <kai.magalhaes.324268777@msft.cesae.pt>
2026-05-15 09:13:16 +0100

    Descarte partes do js

--- Livraria-Hub/public/js/inicio.js
+++ Livraria-Hub/public/js/inicio.js
@@ -1,6 +1,6 @@
 const lst = document.getElementById("lstLiv");
 const bsc = document.getElementById("nmrBusca");
-const btnBsc = document.getElementById("btnBusca"); 
+const btnBsc = document.getElementById("btnBusca");
 const selecEditora = document.getElementById("filtroEditora");
 const selecAno = document.getElementById("filtroAno");
 const selecTipo = document.getElementById("filtroTipo");
@@ -9,14 +9,7 @@ const tema = document.getElementById("btnTema");
 const btnLimpar = document.getElementById("btnLimpar");
 const contadorLivros = document.getElementById("contadorLivros");
 
-const dds = [
-  { tit: "Matemática A 10º", editora: "1", ano: "6", tipo: "4", discip: "2", prc: "18.50", img: "" },
-  { tit: "Português 11º Ano", editora: "1", ano: "7", tipo: "4", discip: "1", prc: "16.90", img: "" },
-  { tit: "Física e Química", editora: "2", ano: "7", tipo: "4", discip: "6", prc: "19.00", img: "" },
-  { tit: "O Senhor dos Anéis", editora: "3", ano: "", tipo: "1", discip: "7", prc: "22.00", img: "" },
-  { tit: "Ciências Naturais 8º", editora: "2", ano: "4", tipo: "4", discip: "5", prc: "14.50", img: "" },
-  { tit: "Caderno de Inglês 9º", editora: "1", ano: "5", tipo: "5", discip: "4", prc: "9.90", img: "" },
-];
+const dds = [];
 
 function render(arr) {
   if (arr.length === 0) {
@@ -41,7 +34,6 @@ function render(arr) {
   `).join("");
 }
 
-// --------------------------------------PARTE DOS FILTROS----------------------------------
 function filtrar() {
   const txt = bsc.value.toLowerCase();
   const editora = selecEditora.value;
@@ -71,10 +63,9 @@ function limparFiltros() {
   filtrar();
 }
 
-// ------------------------------------BARRA DE PESQUISA DA NAVBAR--------------------------
-btnBsc.onclick = filtrar; 
+btnBsc.onclick = filtrar;
 
-bsc.onkeydown = (e) => { 
+bsc.onkeydown = (e) => {
   if (e.key === "Enter") {
     filtrar();
   }
@@ -86,7 +77,6 @@ selecTipo.onchange = filtrar;
 selecDiscip.onchange = filtrar;
 btnLimpar.onclick = limparFiltros;
 
-// --------------------------------------BOTAO TEMA----------------------------------
 tema.onclick = () => {
   const elemento = document.documentElement;
   const nvo = elemento.getAttribute("data-bs-theme") === "dark" ? "light" : "dark";
